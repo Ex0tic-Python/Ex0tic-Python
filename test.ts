c86e69ec94d1aae5c12f00c3746155692d239a72
Ex0tic-Python <80157573+Ex0tic-Python@users.noreply.github.com>
2021-07-24 00:12:06 -0500

    Deleted file
    Was testing

--- test.ts
+++
@@ -1,8 +0,0 @@
-// Between any two numbers
-Math.floor(Math.random() * (max - min + 1)) + min;
-
-// Between 0 and max
-Math.floor(Math.random() * (max + 1));
-
-// Between 1 and max
-Math.floor(Math.random() * max) + 1;
