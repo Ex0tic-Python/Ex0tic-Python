78cc11b7123d1922a9140512f86fd32e47716cf2
Ex0tic-Python <80157573+Ex0tic-Python@users.noreply.github.com>
2021-07-24 00:08:53 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-Testing the top langs
+// Between any two numbers
+Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Between 0 and max
+Math.floor(Math.random() * (max + 1));
+
+// Between 1 and max
+Math.floor(Math.random() * max) + 1;
